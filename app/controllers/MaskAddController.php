7116aed0fa3bbbc4715c0e3c41b4396370900ae8
Chuhlomin <chuhlomin.d@mail.ru>
2022-05-26 08:14:23 +0300

    main

---
+++ app/controllers/MaskAddController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace controllers;
+
+use core\Twig;
+use models\MaskAddModel;
+
+class MaskAddController extends AppController
+{
+    private $model;
+
+    public function indexAction() {
+        $this->checkAuthorization();
+
+        $this->setMeta('Выбор системы');
+        $this->model = new MaskAddModel();
+
+        
+
+        if(isset($_REQUEST['select_value'])){
+            echo print_r($_REQUEST['select_value']);
+            echo print_r('Нет');
+        }
+
+    }
+
+
+    /* Дима */
+    public function addtuAction() {
+        $this->checkAuthorization();
+        $this->setMeta('Добавление защит на ТУ');
+        $this->model = new MaskAddModel();
+
+        $this->setAddtuVarsToTwig();
+    }
+
+    public function addnpsAction() {
+        $this->checkAuthorization();
+        $this->setMeta('Добавление защит на НПС');
+        $this->model = new MaskAddModel();
+        if(isset($_REQUEST['id_for_sub'])) {
+            /*  $this->model->getSubdivision($_REQUEST['id_for_sub']); */
+             $this->model->getObject($_REQUEST['id_for_sub']);
+             $isAjax = true;
+         } 
+
+        if(isset($_REQUEST['select_value'])){
+            echo print_r($_REQUEST['select_value']);
+            echo print_r('Нет');
+        }
+
+        $this->setAddnpsVarsToTwig();
+    }
+    /* Дима */
+
+    public function addluAction() {
+        $this->checkAuthorization();
+        $this->setMeta('Добавление защит на ЛУ');
+        $this->model = new MaskAddModel();
+
+        $this->setAddluVarsToTwig();
+    }
+
+    public function setAddtuVarsToTwig() {
+        $arr = $this->model->getAddtuVarsToTwig();
+        Twig::addVarsToArrayOfRender($arr);
+    }
+
+    public function setAddnpsVarsToTwig() {
+        $arr = $this->model->getAddnpsVarsToTwig();
+        Twig::addVarsToArrayOfRender($arr);
+    }
+
+    
+    public function setAddluVarsToTwig() {
+        $arr = $this->model->getAddluVarsToTwig();
+        Twig::addVarsToArrayOfRender($arr);
+    }
+
+    public function setIndexVarsToTwig(){
+        $arr = $this->model->getIndexVarsToTwig();
+        Twig::addVarsToArrayOfRender($arr);
+    }
+}
---
+++ config/init.php
@@ -0,0 +1,19 @@
+<?php
+
+define("ROOT", $_SERVER['DOCUMENT_ROOT']);
+define("PATH", 'http://' . $_SERVER['HTTP_HOST']);
+const DEBUG = 1;
+const WWW = ROOT . '/public';
+const ADMIN = ROOT . '/public/admin';
+const APP = ROOT . '/app';
+const CORE = ROOT . '/vendor/core';
+const LIBS = ROOT . '/vendor/core/libs';
+const CACHE = ROOT . '/tmp/cache';
+const CONF = ROOT . '/config';
+const VIEW = APP . '/views';
+const ERROR_PAGE = VIEW . '/errors/404.php';
+const LAYOUT = 'default';
+const HTTP = 'http';
+const NAME_WEBSITE = 'trans';
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
